1f4680cbfd9bbbc8dd3a0cf6c0a89c65600fc923
SimbaLeRoiDeLaJungle <mathieu.gallo95@gmail.com>
2022-05-27 11:02:57 +0200

    ajustement de quelques paramètres pour l'effet de l'attaque

--- Assets/Simba/Scirpts/AttackEffectScript.cs
+++ Assets/Simba/Scirpts/AttackEffectScript.cs
@@ -6,13 +6,14 @@ public class AttackEffectScript : MonoBehaviour
 {
     [SerializeField] ParticleSystem ps;
     SpriteRenderer sr;
+    [SerializeField] float duration;
     void Start()
     {
        sr = GetComponent<SpriteRenderer>();
     }
 
     public IEnumerator Play(){
-        yield return new WaitForSeconds(0.1f);
+        yield return new WaitForSeconds(duration);
         sr.color = Color.clear;
         ps.Play();
         while (ps.IsAlive())
