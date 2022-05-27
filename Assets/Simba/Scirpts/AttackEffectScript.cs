006a0b0136568d712d109ab7c44d81739a164817
SimbaLeRoiDeLaJungle <mathieu.gallo95@gmail.com>
2022-05-27 10:25:36 +0200

    ajout d'un effet pour les attaques

---
+++ Assets/Simba/Scirpts/AttackEffectScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackEffectScript : MonoBehaviour
+{
+    [SerializeField] ParticleSystem ps;
+    SpriteRenderer sr;
+    void Start()
+    {
+       sr = GetComponent<SpriteRenderer>();
+    }
+
+    public IEnumerator Play(){
+        yield return new WaitForSeconds(0.1f);
+        sr.color = Color.clear;
+        ps.Play();
+        while (ps.IsAlive())
+        {
+            yield return null;
+        }
+        Destroy(this.gameObject, 0.1f);
+        yield return new WaitForSeconds(0.1f);
+    } 
+
+
+}
--- Assets/Simba/Scirpts/GfxUpdater.cs
+++ Assets/Simba/Scirpts/GfxUpdater.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 namespace Simba{
+    // Pour comprendre cette classe il faut aller faire un tour ici : Tu selectionne le gameObject "Player/Gfx" et tu va dans widow/animation ici tu peux looker animator et animation pour voir comment c'est construit
     public class GfxUpdater : MonoBehaviour
     {
         Animator animator;
+        [SerializeField] GameObject attackEffectPrefab;
+        [SerializeField] Vector2 effectRelativePosition;
         void Start()
         {
             animator = GetComponent<Animator>();
@@ -23,6 +26,25 @@ namespace Simba{
             }
             if(attack){
                 animator.SetTrigger("attack");
+                Vector2 position = Vector2.zero;
+                bool direction = animator.GetBool("direction");
+                if(direction){
+                    position = effectRelativePosition + (Vector2)transform.position;
+                }
+                else{
+                    position = new Vector2(-effectRelativePosition.x,effectRelativePosition.y) + (Vector2)transform.position;
+                }
+                if(chargeTimer.Time >= chargeTimer.MaxTime*0.5f){
+                    GameObject go = Instantiate(attackEffectPrefab , (Vector3)position, Quaternion.identity);
+                    var script = go.GetComponent<AttackEffectScript>();
+                    if(direction){
+                        Vector3 scale = go.transform.localScale;
+                        go.transform.localScale = new Vector3(-scale.x, scale.y, 1);
+                    }
+                    if(script != null){
+                        StartCoroutine(script.Play());
+                    }
+                }
             }
 
             // on verifie si le calques n°2(ici c'est pas les calques d'affichage graphique, mais les calques de l'animator, voir dans widow/animation/animator) 
@@ -35,6 +57,8 @@ namespace Simba{
             else{
                 string clipName = info[0].clip.name;
                 isAttacking = clipName.Contains("End"); 
+                // J'ai découper les animations d'attaque en deux SaberLeft et SaberLeftEnd, pareil pour Right donc ici je vérifie si on est entrain 
+                // de charger (ne contient pas End dans le nom) où entrain d'attaquer c'est un peu a la rache cette partie ^^. 
             }
 
         }
